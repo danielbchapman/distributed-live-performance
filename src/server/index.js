89a25971b4582365a8eb66f57fdd8cee15798bb1
Daniel B. Chapman <chapman@danielbchapman.com>
2020-10-12 19:45:49 -0600

    - added a basic express server and a demo websocket to start building the communication library around it.

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,31 @@
-console.log('exiting...')
+const SOCKET_PORT = 53007
+const SERVER_PORT = 9001
+console.log('----------starting node server on 9001, websockets on 53007')
+import express from 'express'
+import fs from 'fs-extra'
+import http from 'http'
+import ws from 'ws'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Express Server Runnning....')
+})
+
+const server = app.listen(9001)
+
+const wsServer = new ws.Server({ noServer: true})
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message) )
+  socket.send('server-message')
+})
+
+wsServer.on('message', socket => {
+  console.log(JSON.stringify(socket, null, 2))
+  socket.send('yo ho ho')
+})
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request)
+  })
+})
--- src/web/cues/Cues.js
+++ src/web/cues/Cues.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import Layout from './../components/Layout'
+import {
+  Button
+} from '@material-ui/core'
+
 class Cues extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.didConnect = false
+    this.client = null
+  }
+  websocketConnect() {
+    if(!this.didConnect) {
+      this.client = new WebSocket('ws://localhost:9001')
+      let client = this.client
+
+      client.addEventListener('open', event => {
+        client.send('Hi I connected')
+      })
+
+      client.addEventListener('message', event => {
+        alert(`Message from server: ${event.data}`)
+      })
+
+      this.didConnect = true
+    } else {
+      this.client.send('Yo Yo Yo.')
+    }
+  }
+
   render() {
     return (
       <Layout>
         <h1>CUE SHEET</h1>
+        <Button
+          variant='contained'
+          onClick={x => {
+            this.websocketConnect()
+          }}
+        >
+          Say hi to the server
+        </Button>
       </Layout>
     )
   }
